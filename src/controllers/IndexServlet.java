083c9b959e44414b52d6209701801c160b3c8cf6
yosh <icehockey33goalie@yahoo.co.jp>
2020-04-05 16:43:33 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,8 +1,10 @@
 package controllers;
+
 import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,8 +37,12 @@ public class IndexServlet extends HttpServlet {
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
                                    .getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 }
